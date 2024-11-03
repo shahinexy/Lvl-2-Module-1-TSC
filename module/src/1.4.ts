950274bac3385e3b7ea6539ecbb335f68de55747
shahinexy <shahinexy@gmail.com>
2024-11-03 23:09:51 +0600

    tsc data type

---
+++ module/src/1.4.ts
@@ -0,0 +1,44 @@
+// Basic Data Type
+
+// ======= premetive data type ======
+
+// String
+let firstName : string = "Shahin"
+
+// Number 
+let roll : number = 1
+
+// boolean 
+let isAdmin : boolean = true
+
+// undefined 
+let x : undefined = undefined
+
+// null
+let y : null = null
+
+// anytype
+let d;
+
+d = 123
+d = 'alam'
+d= false
+
+// type of
+let b: number;
+
+b = 123
+
+
+// ============ Non premetive datatype ==========
+// Array
+let friends : string[] = ['shifat', 'rifat']
+
+let rollList : number[] = [1,2,3]
+
+// Tuple --> array --> order --> type of value
+let coordinates : [number, number] = [1,2]
+
+let ageName : [number, string, boolean] = [20, 'sakib', true]
+
+ageName[0] = 21
