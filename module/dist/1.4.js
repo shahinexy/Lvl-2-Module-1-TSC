74d0ce7f9c2577572d69e3a4b79c885e16305e91
shahinexy <shahinexy@gmail.com>
2024-11-03 23:12:01 +0600

    dist created

---
+++ module/dist/1.4.js
@@ -0,0 +1,29 @@
+"use strict";
+// Basic Data Type
+// ======= premetive data type ======
+// String
+let firstName = "Shahin";
+// Number 
+let roll = 1;
+// boolean 
+let isAdmin = true;
+// undefined 
+let x = undefined;
+// null
+let y = null;
+// anytype
+let d;
+d = 123;
+d = 'alam';
+d = false;
+// type of
+let b;
+b = 123;
+// ============ Non premetive datatype ==========
+// Array
+let friends = ['shifat', 'rifat'];
+let rollList = [1, 2, 3];
+// Tuple --> array --> order --> type of value
+let coordinates = [1, 2];
+let ageName = [20, 'sakib', true];
+ageName[0] = 21;
