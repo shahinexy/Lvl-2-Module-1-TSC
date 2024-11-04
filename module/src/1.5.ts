d2e1174040a5f9436b22310ba5fe2e3431d3416f
shahinexy <shahinexy@gmail.com>
2024-11-04 22:23:14 +0600

    tsc object

---
+++ module/src/1.5.ts
@@ -0,0 +1,13 @@
+// Reference Type --> Object 
+
+const user: {
+    firstName : string;
+    lastNmae? : string; // optional type
+    isStudent : boolean;
+    course : 'Level 2 Complete Web Development Course' // type --> Literal type
+} = {
+    firstName : 'Shahin',
+    // lastNmae : 'Alam',
+    isStudent : true,
+    course : 'Level 2 Complete Web Development Course'
+}
