0df2168f5232470afbf502af2e2032f6735353c9
shahinexy <shahinexy@gmail.com>
2024-11-04 22:40:48 +0600

    function in tsc

---
+++ module/src/1.6.ts
@@ -0,0 +1,10 @@
+// Learning Function
+
+// Normal Function
+function add(num1 : number, num2: number): number{
+    return num1 + num2
+}
+
+// add(2, 'shahin') error
+// add(2, true) error
+add(5,10)
