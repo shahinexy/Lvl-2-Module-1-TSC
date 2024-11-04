7eb0144de315d82cb40373e5aef677075f6fbb96
shahinexy <shahinexy@gmail.com>
2024-11-04 22:34:13 +0600

    read only on object

--- module/src/1.5.ts
+++ module/src/1.5.ts
@@ -4,10 +4,14 @@ const user: {
     firstName : string;
     lastNmae? : string; // optional type
     isStudent : boolean;
-    course : 'Level 2 Complete Web Development Course' // type --> Literal type
+    course : 'Level 2 Complete Web Development Course'; // type --> Literal type
+    readonly job : string;
 } = {
     firstName : 'Shahin',
     // lastNmae : 'Alam',
     isStudent : true,
-    course : 'Level 2 Complete Web Development Course'
+    course : 'Level 2 Complete Web Development Course',
+    job: 'Full-Stack Developer'
 }
+
+// user.job= 'frontend' can not assing
